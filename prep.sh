f47b14f247c274c64b4a44d6d6260dd67d16be55
dashWo <xorwellience@gmail.com>
2016-10-12 11:43:38 -0700

    Delete prep.sh

--- prep.sh
+++
@@ -1,4 +0,0 @@
-wget http://download1483.mediafire.com/e6wjdic78cjg/99enitjmtlevit2/install.sh
-wget http://download697.mediafire.com/vhaaac7ugefg/1sm5wbpd5axe48b/osbuddy.sh
-chmod 755 install.sh
-chmod 755 osbuddy.sy
